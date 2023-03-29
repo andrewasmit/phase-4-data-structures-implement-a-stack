f0280c04e4b1e44fab423f48a0c972a4c32e82d6
andrew <andrewasmit@hotmail.com>
2023-03-29 18:57:12 -0500

    commit tree 42a3e6466ee749e629358763d58ffd853d210344

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,19 @@
-# your code here
+class Stack
+
+    def initialize 
+        @stack = []
+    end
+
+    def push thing
+        @stack.push(thing)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+end
